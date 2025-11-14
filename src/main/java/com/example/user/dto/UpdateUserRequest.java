08649f5f8aa001992b78dd29ad6ae34aadb3933c
Tior931108 <jyj8469@gmail.com>
2025-11-14 19:57:53 +0900

    🐛 fix: [필수 lv.1~lv.3] 유저 정보 수정 기능 수정
    - 현재 비밀번호(필수) 및 새로운 비밀번호 입력 하도록 수정
    - 새로운 비밀번호 입력은 필수는 아님

--- src/main/java/com/example/user/dto/UpdateUserRequest.java
+++ src/main/java/com/example/user/dto/UpdateUserRequest.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class UpdateUserRequest {
 
-    private String currentPassword;  // 현재 비밀번호
-    private String newPassword;      // 새 비밀번호
+    private String currentPassword;  // 현재 비밀번호 (필수)
+    private String newPassword;      // 새 비밀번호 (산텍)
     private String nickname;
 }
